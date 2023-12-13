86d3610ea6d60c55264a3932a0f2a150c391740e
Gabriel do Carmo Vieira <gabrielcarmovieira16@gmail.com>
2023-12-12 23:38:16 -0300

    chore: update dev dependencies and remove project files

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+const LEVEL = {
+  OFF: 0,
+  WARN: 1,
+  ERROR: 2,
+};
+
+/**
+ * @type {import('eslint').ESLint.ConfigData}
+ */
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  overrides: [
+    {
+      env: { node: true },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: { sourceType: 'script' },
+    },
+  ],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  ignorePatterns: ['node_modules', 'dist', 'coverage'],
+  rules: {
+    'no-unused-vars': [LEVEL.ERROR, { argsIgnorePattern: '_' }],
+  },
+};
